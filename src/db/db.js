1cd8cd9b3374625155ca06b8ae46b7421fc5a31e
Jatin <xjatin83@gmail.com>
2026-02-08 19:10:42 +0530

    Commit

--- src/db/db.js
+++ src/db/db.js
@@ -1,11 +1,9 @@
-  const sqlite3 = require('sqlite3').verbose();
+const sqlite3 = require("sqlite3").verbose();
 
-  const db = new sqlite3.Database('./royalty.db');
+const db = new sqlite3.Database("./royalty.db");
 
-
-  db.serialize(() => {
-
-    db.run(`CREATE TABLE authors (
+db.serialize(() => {
+  db.run(`CREATE TABLE IF NOT EXISTS authors (
       id INTEGER PRIMARY KEY,
       name TEXT,
       email TEXT,
@@ -13,28 +11,27 @@
       ifsc TEXT
     )`);
 
-    db.run(`CREATE TABLE books (
+  db.run(`CREATE TABLE IF NOT EXIST books (
       id INTEGER PRIMARY KEY,
       title TEXT,
       author_id INTEGER,
       royalty INTEGER
     )`);
 
-    db.run(`CREATE TABLE sales (
+  db.run(`CREATE TABLE IF NOT EXIST sales (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       book_id INTEGER,
       quantity INTEGER,
       sale_date TEXT
     )`);
 
-    db.run(`CREATE TABLE withdrawals (
+  db.run(`CREATE TABLE IF NOT EXIST withdrawals (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       author_id INTEGER,
       amount INTEGER,
       status TEXT,
       created_at TEXT
     )`);
+});
 
-  });
-
-  module.exports = db;
+module.exports = db;
